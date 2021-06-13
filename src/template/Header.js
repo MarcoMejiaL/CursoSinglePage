ab3c120576f613423e95694d1d1f8853c2fde056
Marco Mejia <tomatin77@gmail.com>
2021-06-12 23:23:53 -0500

    se empezo y se adelanto el curso de Single Application

---
+++ src/template/Header.js
@@ -0,0 +1,26 @@
+const Header = ()=>{
+    const view =`
+        <div class="Header-main">
+            <div class="Header-logo">
+               <h1>
+                   <a href="/">
+                    100tifi.co
+                    </a>
+               </h1>     
+
+            </div>
+            <div class="Header-nav">
+                <a href="#/abaut/">
+                    Abaut
+                </a>
+            </div>
+
+
+        </div>
+    
+    
+    
+    `;
+    return view;
+}
+export default Header;
